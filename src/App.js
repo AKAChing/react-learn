cc33f545431e7164d034900f6af65ab479be6d11
akaching <akaching@foxmail.com>
2024-06-15 01:41:19 +0800

    edit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { useState, useEffect, useRef } from "react";
-import Arco from '@arco-design/mobile-react';
-function App() {
-  
-  let form = {
-    name: "111",
-    radio1: 1,
-    select1: 1,
-    switch1: "checked",
-  };
-  const formRef = useRef();
-  let [formData, setFormData] = useState(form);
-  const onValuesChange = (changeValue, values) => {
-    setFormData({ ...formData, ...values });
-    // console.log('onValuesChange: ', changeValue, values);
-  };
-  useEffect(() => {
-    console.log(formData);
-  }, [formData]);
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import { useState, useEffect, useRef } from 'react'
+import './css/index.css'
+export default function App() {
+	const [count, setCount] = useState(0)
+	const [user, setUser] = useState({
+		name: 'zhangsan',
+		age: 18,
+	})
+	const handleClick = () => {
+		// setCount(count + 1)
+		// console.log(count);
+		setUser({
+			...user,
+			name: user.age + 1,
+		})
+	}
+	return (
+		<div className="App">
+			<div class="flex">
+				<div class="flex-none w-14 h-14">01</div>
+				<div class="flex-initial w-64 ...">02</div>
+				<div class="flex-initial w-32 ...">03</div>
+			</div>
+		</div>
+	)
 }
-
-export default App;
