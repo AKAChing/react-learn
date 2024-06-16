b6a3d6fd1a16b3553fba115b15d37563554762ea
akaching <akaching@foxmail.com>
2024-06-16 20:03:48 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import { useContext, useState, useEffect } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import CommentList from './components/CommentList'
 import ReactiveInput from './components/ReactiveInput'
+import InnerInput from './components/InnerInput'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
-
+export const MyContext = createContext()
 export default function App() {
 	// let now = dayjs().format('MM-DD HH:mm')
 	// let fromNow = dayjs('2024-06-17').fromNow()
 	// console.log(now);
-	let [str, setStr] = useState('')
-	// const akaching = useContext('akaching')
+	let [str, setStr] = useState('888')
+	// const AkachingContext = createContext()
 	const handleChange = e => {
 		setStr(e)
 		// console.log(str)
 	}
 	const myfn = () => {
-		console.log('myfn');
+		console.log('myfn')
 	}
 	useEffect(() => {
-		console.log(str)
+		// console.log(str)
 		// inputRef.current.focus()
 		// console.log('useEffect', inputValue)
 	}, [str])
 	return (
 		<div className="App">
 			<div>{str}</div>
-			<ReactiveInput
-				inputChange={handleChange}
-				name={new Date()}
-				defaultValue={'akaching'}
-				arr={[1,2,3]}
-				obj={{name:'akaching', age: 18}}
-				span={<span className='text-white'>发送</span>}
-			>
-				<div className="text-black"><span>akaching</span></div>
-			</ReactiveInput>
+			<MyContext.Provider value={{str, setStr}}>
+				<ReactiveInput
+					inputChange={handleChange}
+					name={new Date()}
+					defaultValue={'akaching'}
+					arr={[1, 2, 3]}
+					obj={{ name: 'akaching', age: 18 }}
+					span={<span className="text-white">发送</span>}
+				>
+					{/* <div className="text-black"><span>akaching</span></div> */}
+					<InnerInput></InnerInput>
+				</ReactiveInput>
+			</MyContext.Provider>
+
 			<CommentList str={str} myfn={myfn}></CommentList>
-			
 		</div>
 	)
 }
--- src/components/ReactiveInput.js
+++ src/components/ReactiveInput.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export default function ReactiveInput(props) {
 	let [inputValue, setInputValue] = useState(props.defaultValue || '')
 	const inputRef = useRef(null)
-	// console.log(props);
+	// console.log(props)
 	const getRef = () => {
 		props.inputChange(inputValue)
 		// inputChange(inputValue)
@@ -20,30 +20,33 @@ export default function ReactiveInput(props) {
 		// console.log('useEffect', inputValue)
 	}, [inputValue])
 	return (
-		<div className="flex mx-auto h-14 p-2">
-			<input
-				className="border flex-1 p-2"
-				ref={inputRef}
-				type="text"
-				value={inputValue}
-				onChange={e => change(e)}
-				placeholder="please input"
-			/>
-			<button
-				className="flex justify-center items-center text-white bg-sky-600 w-36"
-				onClick={getRef}
-			>
-				{props.span}
-				{/* {props.children} */}
-				{/* 发送 */}
-			</button>
+		<div>
+			<div className="flex mx-auto h-14 p-2">
+				<input
+					className="border flex-1 p-2"
+					ref={inputRef}
+					type="text"
+					value={inputValue}
+					onChange={e => change(e)}
+					placeholder="please input"
+				/>
+				<button
+					className="flex justify-center items-center text-white bg-sky-600 w-36"
+					onClick={getRef}
+				>
+					{props.span}
+					{/* {props.children} */}
+					{/* 发送 */}
+				</button>
+			</div>
+
+			<div>{props.children}</div>
 		</div>
 	)
 }
 
-
 ReactiveInput.propTypes = {
 	span: PropTypes.element,
 	inputChange: PropTypes.func.isRequired,
-	defaultValue: PropTypes.string
+	defaultValue: PropTypes.string,
 }
